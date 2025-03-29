91bd12dde6c860ee4f0666444a4f75188b963412
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-03-29 12:45:42 -0300

    feat: adiciona interceptor de token expirado

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,23 @@
 import axios from "axios";
+import { useAuthStore } from "../store/use-auth-store";
+import { useNavigate } from 'react-router-dom';
 
 export const api = axios.create({
     baseURL: 'http://192.168.0.84:3000'
 })
+
+// Interceptor para verificar erro de token expirado
+// ===================================================
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 401) {
+
+            const { logout } = useAuthStore.getState();
+            const navigate = useNavigate()
+            logout();
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    }
+);
