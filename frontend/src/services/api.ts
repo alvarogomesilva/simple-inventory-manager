8477a96e2709eb277fa5a97154288211a9d356bb
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-03-29 12:49:19 -0300

    feat: interceptor para adicionar o token das requisicoes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -6,6 +6,22 @@ export const api = axios.create({
     baseURL: 'http://192.168.0.84:3000'
 })
 
+
+// Interceptor para adicionar o token nas requisições
+// ====================================================
+api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('@u');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
 // Interceptor para verificar erro de token expirado
 // ===================================================
 api.interceptors.response.use(
