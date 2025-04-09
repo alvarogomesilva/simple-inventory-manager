18b00753de9ca32daeb0cc7c4fe7d14b66e85ac0
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-04-09 19:16:20 -0300

    feat: cria novas categorias pelo frontend

--- frontend/src/components/ModalCategories/index.tsx
+++ frontend/src/components/ModalCategories/index.tsx
@@ -1,3 +1,6 @@
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
+import toast from "react-hot-toast";
 
 interface ModalProps {
   isOpen: boolean;
@@ -5,27 +8,44 @@ interface ModalProps {
 }
 
 export function ModalCategories({ isOpen, onClose }: ModalProps) {
+  const [name, setName] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = async (e: FormEvent) => {
+    setIsLoading(true)
+    e.preventDefault()
+
+    try {
+      await api.post('/categories', { name })
+      setName('')
+      toast.success('Categoria salva com sucesso!')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <div
       id="crud-modal"
       tabIndex={-1}
-      className={`${
-        isOpen ? "flex" : "hidden"
-      } fixed inset-0 z-[999] justify-center items-center overflow-y-auto overflow-x-hidden`}
+      className={`${isOpen ? "flex" : "hidden"
+        } fixed inset-0 z-[999] justify-center items-center overflow-y-auto overflow-x-hidden`}
     >
-      {/* Overlay escuro e translúcido */}
+
       <div
         className="absolute inset-0 bg-black/50 backdrop-blur-sm"
         onClick={onClose}
       />
 
-      {/* Modal container */}
+
       <div className="relative z-10 p-4 w-full max-w-md max-h-full">
         <div className="bg-white dark:bg-gray-700/70 backdrop-blur-md rounded-lg shadow-lg border border-gray-300 dark:border-gray-600">
           {/* Header */}
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200 dark:border-gray-600">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Create New Product
+              Nova categoria
             </h3>
             <button
               onClick={onClose}
@@ -50,93 +70,36 @@ export function ModalCategories({ isOpen, onClose }: ModalProps) {
             </button>
           </div>
 
-          {/* Form */}
-          <form className="p-4 md:p-5">
+          <form className="p-4 md:p-5 flex flex-col">
             <div className="grid gap-4 mb-4 grid-cols-2">
               <div className="col-span-2">
                 <label
                   htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Name
+                  Nome
                 </label>
                 <input
                   type="text"
                   name="name"
                   id="name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
-                  placeholder="Type product name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="bg-white border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-blue-600 dark:border-blue-500 dark:text-white"
+                  placeholder="Digite uma nova categoria"
                   required
                 />
-              </div>
-
-              <div className="col-span-2 sm:col-span-1">
-                <label
-                  htmlFor="price"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Price
-                </label>
-                <input
-                  type="number"
-                  name="price"
-                  id="price"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
-                  placeholder="$2999"
-                  required
-                />
-              </div>
-
-              <div className="col-span-2 sm:col-span-1">
-                <label
-                  htmlFor="category"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Category
-                </label>
-                <select
-                  id="category"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
-                >
-                  <option value="">Select category</option>
-                  <option value="TV">TV/Monitors</option>
-                  <option value="PC">PC</option>
-                  <option value="GA">Gaming/Console</option>
-                  <option value="PH">Phones</option>
-                </select>
-              </div>
-
-              <div className="col-span-2">
-                <label
-                  htmlFor="description"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Product Description
-                </label>
-                <textarea
-                  id="description"
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
-                  placeholder="Write product description here"
-                ></textarea>
               </div>
             </div>
 
+
             <button
+              onClick={handleSubmit}
               type="submit"
-              className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white text-center mt-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
             >
-              <svg
-                className="me-1 -ms-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Add new product
+
+              {isLoading ? 'Carregando...': 'Salvar'}
             </button>
           </form>
         </div>
